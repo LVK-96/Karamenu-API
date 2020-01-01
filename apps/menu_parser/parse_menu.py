733c8c3ffac3cfaf7d4d69b4a1a65ea01a45df1a
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-01-01 17:53:10 +0200

    Return empty array if menu fetching fails

--- apps/menu_parser/parse_menu.py
+++ apps/menu_parser/parse_menu.py
@@ -15,18 +15,18 @@ def parse_menu(restaurant, d):
         try:
             courses = sodexo.parse_courses(menu["courses"])
         except KeyError:
-            return Menu.create(restaurant, d, "{}")
+            return Menu.create(restaurant, d, [])
     elif restaurant.name in api_lookup.fazer:
         delta = (d - date.today()).days
         if d < date.today() or delta > 7:
             # Fazer api only has the menus for the current week
-            return Menu.create(restaurant, d, "{}")
+            return Menu.create(restaurant, d, [])
         menu = json.loads(fazer.get_json(restaurant.name))
         try:
             courses = fazer.parse_courses(
                     menu["MenusForDays"][delta]["SetMenus"])
         except KeyError:
-            return Menu.create(restaurant, d, "{}")
+            return Menu.create(restaurant, d, [])
 
     courses = CourseSerializer(courses, many=True).data
     new_menu = Menu.create(restaurant, d, courses)
